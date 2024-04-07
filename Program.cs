2cd9eaaef0fa92ffacc109517ac0be86abe56fd6
subhan <mehdiyevsubhan1@gmail.com>
2024-04-07 17:45:36 +0400

    Feature: Reading email details from excel file is added

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
@@ -7,6 +9,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using ExcelDataReader;
 
 namespace MailBomber
 {
@@ -14,20 +17,47 @@ namespace MailBomber
     {
         static void Main(string[] args)
         {
-            string[] toMails = {"mehdiyevsubhan1@gmail.com","mehdiyevsubhan00@gmail.com","kamran.azizov004@gmail.com" };
+            //Read email details from excel file
+            var mailsDictionary = new Dictionary<int, object[]>();
 
+            using (FileStream fileStream = File.OpenRead("Mails.xlsx"))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(fileStream))
+                {
+                    var config = new ExcelDataSetConfiguration
+                    {
+                        ConfigureDataTable = _ => new ExcelDataTableConfiguration
+                        {
+                            UseHeaderRow = false
+                        }
+                    };
+
+                    var mailsDataSet = reader.AsDataSet(config);
+                    var mailDataTable = mailsDataSet.Tables[0];
+
+                    for (int i = 1; i < mailDataTable.Rows.Count; i++)
+                    {
+                        mailsDictionary.Add(i, mailDataTable.Rows[i].ItemArray);
+                    }
+                }
+            }
+
+            //Create a new emails
             string fromMail = "thedotnetchannelsender22@gmail.com";
             string fromPassword = "lgioenhkvchemfkrw";
 
-            //Create a new email
+            List<MailMessage> messages = new List<MailMessage>();
             MailMessage message = new MailMessage();
             message.From = new MailAddress(fromMail);
-            message.Subject = "Test mail from Subhan";
-            foreach (string mail in toMails)
+
+            foreach (var mailDetails in mailsDictionary.Values)
             {
-                message.To.Add(new MailAddress(mail));
+                message.To.Add(new MailAddress(mailDetails[0].ToString()));
+                message.Subject = mailDetails[1].ToString();
+                message.Body = mailDetails[2].ToString();
+
+                messages.Add(message);
             }
-            message.Body = "Test body from Subhan";
 
             //SMTP configuration
             SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
@@ -35,12 +65,14 @@ namespace MailBomber
             smtpClient.Credentials = new NetworkCredential(fromMail, fromPassword);
             smtpClient.EnableSsl = true;
 
-            //Sending message
+            //Sending messages
             try
             {
-                for (int i = 0; i < 3; i++)
+                for (int i = 0; i < messages.Count; i++)
                 {
                     smtpClient.Send(message);
+                    Console.WriteLine("Email message was sent succesfully.");
+
                     var rnd = new Random();
                     int delayLimit = rnd.Next(5000,10000);
                     Thread.Sleep(delayLimit);
